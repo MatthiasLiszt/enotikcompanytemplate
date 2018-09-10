1b42a7d39701b94fbd60e97c41cfda7ba6185130
matthiasliszt <mliszt.program@gmail.com>
2018-09-10 16:33:32 +0200

    aboutme

--- js/content.js
+++ js/content.js
@@ -3,3 +3,4 @@
 $('#Intro').html(Intro);
 $("#investing").click(function(){$("#Investing").html(Invest);});
 $("#contact").click(function(){$("#Contact").html(Contact);});
+$("#aboutme").click(function(){$("#AboutMe").html(AboutMe);});
