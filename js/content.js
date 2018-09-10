95c4c06ad80b77791ece08b4a0fa11f73e5a2daf
matthiasliszt <mliszt.program@gmail.com>
2018-09-10 18:05:26 +0200

    mediaqueries

--- js/content.js
+++ js/content.js
@@ -1,6 +1,11 @@
 
 
 $('#Intro').html(Intro);
+
 $("#investing").click(function(){$("#Investing").html(Invest);});
 $("#contact").click(function(){$("#Contact").html(Contact);});
 $("#aboutme").click(function(){$("#AboutMe").html(AboutMe);});
+
+$("#investingMobile").click(function(){$("#Investing").html(Invest);});
+$("#contactMobile").click(function(){$("#Contact").html(Contact);});
+$("#aboutmeMobile").click(function(){$("#AboutMe").html(AboutMe);});
